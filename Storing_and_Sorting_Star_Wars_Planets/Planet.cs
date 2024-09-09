21edf804697f0f23e217e529c7b1f30f3f7079f5
breannatoth07 <breannatoth07@gmail.com>
2024-09-09 14:12:51 -0400

    Add project files.

---
+++ Storing_and_Sorting_Star_Wars_Planets/Planet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storing_and_Sorting_Star_Wars_Planets
+{
+    public class Planet
+    {
+        public string Name { get; set; }
+        public int Population { get; set; }
+        public string Region { get; set; }
+
+        public Planet(string name, int population, string region)
+        {
+            Name = name;
+            Population = population;
+            Region = region;
+        }
+
+        public void DisplayPlanetInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Population: {Population}, Region: {Region}");
+        }
+
+        public static void AddPlanet(List<Planet> planets)
+        {
+            Console.Write("Enter the name of the planet: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter the population of the planet: ");
+            int population = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the region of the planet: ");
+            string region = Console.ReadLine();
+
+            planets.Add(new Planet(name, population, region));
+        }
+    }
+}
---
+++ Storing_and_Sorting_Star_Wars_Planets/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storing_and_Sorting_Star_Wars_Planets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Planet> planets = new List<Planet>();
+
+            planets.Add(new Planet("Endor", 30000, "Outer Rim Territories"));
+            planets.Add(new Planet("Naboo", 450000000, "Mid Rim"));
+            planets.Add(new Planet("Tatooine", 200000, "Outer Rim Territories"));
+            planets.Add(new Planet("Coruscant", 100000000, "Core Worlds"));
+
+            Console.WriteLine("List of Star Wars Planets:");
+            foreach (Planet planet in planets)
+            {
+                planet.DisplayPlanetInfo();
+            }
+
+            planets.Sort((p1, p2) => p1.Population.CompareTo(p2.Population));
+
+            Console.WriteLine("\nPlanets sorted by population:");
+            foreach(Planet planet in planets)
+            {
+                planet.DisplayPlanetInfo();
+            }
+
+            bool addingPlanets = true;
+
+            while(addingPlanets)
+            {
+                Console.Write("Do you want to add a new planet? (yes/no): ");
+                string response = Console.ReadLine().ToLower();
+
+                if (response == "yes")
+                {
+                    Planet.AddPlanet(planets);
+                }
+                else
+                {
+                    addingPlanets = false;
+                }
+            }
+
+            planets.Sort((p1, p2) => p1.Population.CompareTo(p2.Population));
+
+            Console.WriteLine("\nUpdated list of planets sorted by population:");
+            foreach (Planet planet in planets)
+            {
+                planet.DisplayPlanetInfo();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
